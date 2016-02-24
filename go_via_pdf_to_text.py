839020bbf28ca6540fee3d9cb563daf539d9cc73
Shaun Dyble <sdyble@splunk.com>
2016-02-24 16:51:52 +1100

    added gio via py file and added .gitignore for the pdf files

--- go_via_pdf_to_text.py
+++ go_via_pdf_to_text.py
@@ -6,7 +6,7 @@ from pdfminer.converter import HTMLConverter
 from pdfminer.layout import LAParams
 from pdfminer.pdfpage import PDFPage
 from cStringIO import StringIO
-
+import os
 
 def convert_pdf_to_txt(path):
     rsrcmgr = PDFResourceManager()
@@ -32,49 +32,39 @@ def convert_pdf_to_txt(path):
     retstr.close()
     return text
 
-results = convert_pdf_to_txt('C:\Users\Shaun\Downloads\statement.pdf')
-results2 = convert_pdf_to_txt('C:\Users\Shaun\Downloads\statement (1).pdf')
-results_split = filter(None, results.split('\n'))
-results_split2 = filter(None, results2.split('\n'))
+pdf_files = [f for f in os.listdir('.') if os.path.isfile(f)]
+
+for pdf in pdf_files:
+    if pdf.endswith(".pdf"):
+        print pdf
+
+results = convert_pdf_to_txt('statement.pdf')
 
 results_list = filter(None, results.split('\n'))
-results2_list = filter(None, results2.split('\n'))
 
 split_test = results.split('New charges - transaction details')
-split_test2 = results2.split('New charges - transaction details')
 
 split_1 = filter(None, split_test[1].split('Total incl. GST($)'))
-split_12 = filter(None, split_test2[1].split('Total incl. GST($)'))
 
 split_2 = split_1[1].split('Total trips for tag 0914129799')
-split_2_filtered = filter(None, split_2[0].split('\n'))
-
-split_22 = split_12[1].split('Total trips for tag 0914129799')
-split_22_filtered = filter(None, split_22[0].split('\n'))
 
 split_3_docnum_date = filter(None, split_2[0].split('\n'))
-split_3_docnum_date2 = filter(None, split_22[0].split('\n'))
 
 list_length = split_3_docnum_date.__len__()
-list_length2 = split_3_docnum_date2.__len__()
 
 split_4 = split_2[1].split('Other fees, charges and adjustments')
-split_42 = split_22[1].split('Other fees, charges and adjustments')
 
 split_5_time = filter(None, split_4[0].split('\n'))
-split_5_time2 = filter(None, split_42[0].split('\n'))
 
 location = split_5_time[list_length:list_length*2]
-location2 = split_5_time2[list_length:list_length*2]
 
 split6 = split_4[1].split('Total other fees, charges and adjustments ')
-split62 = split_42[1].split('Total other fees, charges and adjustments ')
 
-split7_cost_list = filter(None, split6[1].split('\n'))
-split7_cost_list2 = filter(None, split_42[0].split('\n'))
+#split7_cost_list = filter(None, split6[1].split('\n'))
+split7_cost_list = filter(None, split_4[0].split('\n'))
 
-cost_list = split7_cost_list[0:list_length]
-cost_list2 = split7_cost_list2[list_length2*2:list_length2*3]
+#cost_list = split7_cost_list[0:list_length]
+cost_list= split7_cost_list[list_length*2:list_length*3]
 
 final_list = []
 count = 0
